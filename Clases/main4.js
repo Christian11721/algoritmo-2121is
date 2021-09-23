414c92f9ffe7386d6741a2d974b7299b8552a7be
Christian11721 <christian_1321124087@uptecamac.edu.mx>
2021-09-23 17:30:01 -0500

    clases

---
+++ Clases/main4.js
@@ -0,0 +1,8 @@
+var palabra = parseInt(prompt("ingresa un número")
+for (palabra == 0){
+    palabra= porseint(prompt("ingresa un número"));
+}
+var palabra = a,b,c,d,e,f,j,g,h,i,k,m,n,l,o,p,q,r,s,t,u,w,x,y,z;
+for(var palabra= a,b,c,d,e,f,j,g,h,i,k,m,n,l,o,p,q,r,s,t,u,w,x,y,z; palabra <= 5; palabra++){
+    console.log(palabra += palabra); 
+}  
---
+++ Clases/mains3.js
@@ -0,0 +1,25 @@
+// imprimir todos los dijitos decimales, del 0 al 9 utilizando una repetición.//
+for(var x = 0.1; x <= 10;x++){
+    console.log (x); 
+}
+// ejercicio 2
+for(var i = 100; i <= 199;i++){
+    console.log(i);
+}
+//ejercicio 3
+for (var y=5; y <= 20; y = y + 3) {
+    console.log(y);
+  }
+  //ejercicio 4
+  var x = parseInt(prompt("Ingrese un número"))
+for (x = 2; x <= 5; x++){
+    for(var y=1; y <= 10; y++){
+        console.log(x + "x" + y + "=" - x * y); 
+    } 
+} 
+//ejercicio 5
+
+//ejercicio 6
+for(var suma = 0; suma <= 100; suma++){
+    console.log(suma += suma); 
+} 
