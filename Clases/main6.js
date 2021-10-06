5af706f0ac1268df0181ef46e16b6f8c60b9deec
Christian11721 <christian_1321124087@uptecamac.edu.mx>
2021-10-06 16:14:38 -0500

    while

---
+++ Clases/main6.js
@@ -0,0 +1,23 @@
+var x = 0; 
+ while(x <= 10){
+     console.log(x);    
+     x++; 
+ }
+
+ var i = 1;
+ while(i<10){
+  console.log(i);
+  i=i*3;
+ }
+
+ var i=0; 
+ while (i<=5) { 
+     console.log(+ i);
+     i=i+1;
+ }
+ var numero = 1;
+ while( numero < 21 ) {
+   console.log(numero);
+   numero++;
+ } 
+ 
